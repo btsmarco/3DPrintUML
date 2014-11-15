4da653483d098996740fad3b72e8b1fddbbcee33
Marco Botros <btsmarco@gmail.com>
2014-11-14 19:30:11 -0600

    open and read files

--- script.py
+++ script.py
@@ -2,9 +2,17 @@
 # Author: Marco Botros
 # Objective: Convert UML diagrams (XML format) to Collada for 3D printing (also XML)
 
+import xml.etree.ElementTree as ET
+
 # Import the UML XML file
 # Import Collada template
+UMLFilePath = '../UML/project.xml'
+ColladaFilePath = '../blender/plain.dae'
+OutputFilePath = '../Output/diagram.dae'
 
+UMLFile = open(UMLFilePath,'r')
+ColladaFile = open(ColladaFilePath,'r')
+OutputFile = open( OutputFilePath, 'w')
 
 # Extract the required tags from the XML
 
